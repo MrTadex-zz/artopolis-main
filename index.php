4f4cdb6d579e40ecd3b85f96fc7feca126c05b34
Lij <1sk8rdude@gmail.com>
2019-07-02 18:19:01 +0200

    start of main page

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <!-- Font awesome - free (FA) -->
+    <link rel="stylesheet" href="./src/fa/css/all.min.css">
+    <script src="./src/fa/js/all.min.js"></script>
+
+    <!-- Bootstrap - free (BS) -->
+    <link rel="stylesheet" href="./src/bs/css/bootstrap.min.css">
+    <script src="./src/bs/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <!-- Just an image -->
+    <div class="container">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="./src/custom/img/artopolis-logo-black-square.svg" width="30" height="30" alt="">
+            </a>
+        </nav>
+    </div>
+
+</body>
+
+</html>
